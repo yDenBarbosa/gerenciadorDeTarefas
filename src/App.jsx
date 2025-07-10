ab73d50c3a4eb8fb8fd41dbe27973f68e0ba1d25
yDenBarbosa <denis9195@hotmail.com>
2025-07-10 02:00:59 -0300

    feat:delete button

--- src/App.jsx
+++ src/App.jsx
@@ -28,6 +28,22 @@ function App() {
     },
   ]);
 
+  function onTaskClick(taskId){
+     const newTasks = tasks.map((task) => {
+       if (task.id === taskId) {
+         return {...task, isCompleted: !task.isCompleted};
+       } 
+       return task;
+     });
+     setTasks(newTasks);
+   }
+
+  function onDeleteTaskClick(taskId){
+    const newTasks = tasks.filter((task) => task.id !== taskId)
+    setTasks(newTasks);
+  }
+
+
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
       <div className="w-[500px]">
@@ -35,7 +51,7 @@ function App() {
           Gerenciador de Tarefas
         </h1>
         <AddTask />
-        <Tasks tasks={tasks} />
+        <Tasks tasks={tasks} onTaskClick={onTaskClick} onDeleteTaskClick={onDeleteTaskClick}/>
       </div>
     </div>
   );
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,12 +1,24 @@
+import { ChevronRightIcon, TractorIcon, TrashIcon } from "lucide-react";
+
 function Tasks(props) {
   return (
     <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
-      {props.tasks.map((tasks) => (
-        <li key={tasks.id} className="flex">
-          <button className="bg-slate-400 w-full text-white p-2 rounded-md">
-            {tasks.title}
+      {props.tasks.map((task) => (
+        <li key={task.id} className="flex gap-2">
+          <button 
+          onClick={() => props.onTaskClick(task.id)} 
+          className={`bg-slate-400 w-full text-left text-white p-2 rounded-md ${task.isCompleted && 'line-through'}`}>
+            {task.title}
+            {/* {tasks.isCompleted ? "COMPLETE" : "INCOMPLETE"} */}
           </button>
-          <button>ver detalhes</button>
+          <button className="bg-slate-400 p-2 rounded-md text-white">
+            <ChevronRightIcon />
+          </button>
+          <button 
+            onClick={() => props.onDeleteTaskClick(task.id)}
+            className="bg-slate-400 p-2 rounded-md text-white">
+            <TrashIcon />
+          </button>
         </li>
       ))}
     </ul>
