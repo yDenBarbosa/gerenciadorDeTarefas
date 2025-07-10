3e5e2dbd96ab54267f861de90783bfb479e503fb
yDenBarbosa <denis9195@hotmail.com>
2025-07-10 02:14:48 -0300

    chore:renane of pops

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,12 +1,12 @@
 import { ChevronRightIcon, TractorIcon, TrashIcon } from "lucide-react";
 
-function Tasks(props) {
+function Tasks({tasks, onTaskClick, onDeleteTaskClick}) {
   return (
     <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
-      {props.tasks.map((task) => (
+      {tasks.map((task) => (
         <li key={task.id} className="flex gap-2">
           <button 
-          onClick={() => props.onTaskClick(task.id)} 
+          onClick={() => onTaskClick(task.id)} 
           className={`bg-slate-400 w-full text-left text-white p-2 rounded-md ${task.isCompleted && 'line-through'}`}>
             {task.title}
             {/* {tasks.isCompleted ? "COMPLETE" : "INCOMPLETE"} */}
@@ -15,7 +15,7 @@ function Tasks(props) {
             <ChevronRightIcon />
           </button>
           <button 
-            onClick={() => props.onDeleteTaskClick(task.id)}
+            onClick={() => onDeleteTaskClick(task.id)}
             className="bg-slate-400 p-2 rounded-md text-white">
             <TrashIcon />
           </button>
