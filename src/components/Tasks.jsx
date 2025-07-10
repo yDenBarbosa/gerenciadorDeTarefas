13bea2985cac67697af0f466c6b7edc3151e257f
yDenBarbosa <denis9195@hotmail.com>
2025-07-10 00:51:46 -0300

    feat:button

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,10 +1,15 @@
 function Tasks(props) {
   return (
-    <h1>
+    <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
       {props.tasks.map((tasks) => (
-        <p>{tasks.title}</p>
+        <li key={tasks.id} className="flex">
+          <button className="bg-slate-400 w-full text-white p-2 rounded-md">
+            {tasks.title}
+          </button>
+          <button>ver detalhes</button>
+        </li>
       ))}
-    </h1>
+    </ul>
   );
 }
 
