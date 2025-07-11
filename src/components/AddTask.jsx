1a51e7ded942fa344ed526063b09febf0b997b30
yDenBarbosa <denis9195@hotmail.com>
2025-07-10 21:24:50 -0300

    feat:addTask

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,5 +1,11 @@
 function AddTask() {
-    <h1>Add Task</h1>
+    return (
+        <div className="space-y-4 p-6 bg-slate-200 rounded-md shadow flex flex-col">
+            <input type="text" placeholder="Digite o titulo da tarefa" className="border-slate-300 outline-slate-400 px-4 py-2 rounded-md"/>
+            <input type="text" placeholder="Digite a descrição da tarefa" className="border-slate-300 outline-slate-400 px-4 py-2 rounded-md"/>
+            <button className="bg-slate-500 text-white px-2 py-3 rounded-md font-medium">Adicionar</button>
+        </div>
+    );
 }
 
 export default AddTask
