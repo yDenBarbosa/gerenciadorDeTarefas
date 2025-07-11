77bcc1f1135b497156cbf52fe15c1074cd8abe1f
yDenBarbosa <denis9195@hotmail.com>
2025-07-10 22:53:07 -0300

    feat:routs

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,6 +1,8 @@
 import { ChevronRightIcon, TractorIcon, TrashIcon } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 function Tasks({tasks, onTaskClick, onDeleteTaskClick}) {
+  const navigate = useNavigate()
   return (
     <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
       {tasks.map((task) => (
