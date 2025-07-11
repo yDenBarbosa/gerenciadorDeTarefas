2d6874c652b85d2ea9dda237c3d4f6bbc1c76c29
yDenBarbosa <denis9195@hotmail.com>
2025-07-10 23:31:50 -0300

    - init: começa estilização da página

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,24 +1,39 @@
 import { ChevronRightIcon, TractorIcon, TrashIcon } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 
-function Tasks({tasks, onTaskClick, onDeleteTaskClick}) {
+function Tasks({ tasks, onTaskClick, onDeleteTaskClick }) {
   const navigate = useNavigate()
+  
+  function onSeeDetailsClick(task) {
+    const query = new URLSearchParams();
+    query.set("title", task.title);
+    query.set("description", task.description);
+    navigate(`/task?${query.toString()}`);
+  }
+
   return (
     <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
       {tasks.map((task) => (
         <li key={task.id} className="flex gap-2">
-          <button 
-          onClick={() => onTaskClick(task.id)} 
-          className={`bg-slate-400 w-full text-left text-white p-2 rounded-md ${task.isCompleted && 'line-through'}`}>
+          <button
+            onClick={() => onTaskClick(task.id)}
+            className={`bg-slate-400 w-full text-left text-white p-2 rounded-md ${
+              task.isCompleted && "line-through"
+            }`}
+          >
             {task.title}
             {/* {tasks.isCompleted ? "COMPLETE" : "INCOMPLETE"} */}
           </button>
-          <button className="bg-slate-400 p-2 rounded-md text-white">
+          <button
+            onClick={() => onSeeDetailsClick(task)}
+            className="bg-slate-400 p-2 rounded-md text-white"
+          >
             <ChevronRightIcon />
           </button>
-          <button 
+          <button
             onClick={() => onDeleteTaskClick(task.id)}
-            className="bg-slate-400 p-2 rounded-md text-white">
+            className="bg-slate-400 p-2 rounded-md text-white"
+          >
             <TrashIcon />
           </button>
         </li>
--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -1,13 +1,33 @@
-import { useSearchParams } from "react-router-dom"
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { ChevronLeftIcon } from "lucide-react";
 
 function TaskPage() {
-    const [searchParams] = useSearchParams();
-    const title = searchParams.get("title")
-    const description = searchParams.get("description")
-    return <div className="h-screen w-screen bg-slate-500 p-6">
-        <h1>{title}</h1>
-        <p>{description}</p>
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+  const title = searchParams.get("title");
+  const description = searchParams.get("description");
+  return (
+    <div className="h-screen w-screen bg-slate-500 p-6">
+      <div className="w-[500px] space-y-4">
+        <div className="flex justify-center relative mb-6">
+          <button
+            onClick={() => navigate(-1)}
+            className="absolute left-0 top-0 bottom-0 text-slate-100"
+          >
+            <ChevronLeftIcon />
+          </button>
+          <h1 className="text-3xl text-slate-100 font-bold text-center">
+            Detalhes da Tarefa
+          </h1>
+        </div>
+
+        <div className="bg-slate-200 p-4 rounded-md">
+          <h1 className="text-xl font-bold text-slate-600">{title}</h1>
+          <p className="text-slate-600">{description}</p>
+        </div>
+      </div>
     </div>
+  );
 }
 
-export default TaskPage
+export default TaskPage;
